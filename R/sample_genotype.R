16a9f11a21afc05c6087b2b3fb4b7be7ab824f7e
mkruijver <maarten.kruijver@esr.cri.nz>
2021-11-04 14:54:17 +1300

    Add sampling functions for genotype and offspring

---
+++ R/sample_genotype.R
@@ -0,0 +1,54 @@
+#' @title Sample a genotype according to allele frequencies
+#'
+#' @param freqs Allele frquencies (see \link{read_allele_freqs})
+#' @param loci Character vector of locus names (defaults to names attr. of \code{freqs})
+#' @param label Sample name
+#' @details A genotype is sampled randomly by drawing two alleles from allele frequencies.
+#' @examples
+#' # below we read an allele freqs and sample a genotype
+#' filename <- system.file("extdata","FBI_extended_Cauc.csv",package = "SimMixDNA")
+#' freqs <- read_allele_freqs(filename)
+#' sample_genotype(freqs, loci = c("D3S1358", "vWA"))
+#' @export
+sample_genotype <- function(freqs, loci = names(freqs), label = "U"){
+
+  if (!is.list(freqs)){
+    stop("freqs should be a list")
+  }
+  if (!all(sapply(freqs, is.numeric))){
+    stop("freqs should be a list of numeric vectors")
+  }
+  for (locus in loci){
+    if (!(locus %in% names(freqs))){
+      stop(paste0("freqs not available for locus "), locus)
+    }
+  }
+
+  number_of_loci <- length(loci)
+
+  epg <- data.frame("Sample Name" = rep(label, number_of_loci),
+                    Locus = loci,
+                    Allele1 = character(number_of_loci),
+                    Allele2 = character(number_of_loci),
+                    stringsAsFactors = FALSE, check.names = FALSE)
+
+  for(i_locus in seq_along(loci)){
+    locus <- loci[i_locus]
+
+    f <- freqs[[locus]]
+
+    tryCatch({
+      ab <- names(f)[sort(sample.int(n = length(f),
+                 size = 2, replace = TRUE, prob = f))]
+    }, error = function(e){
+
+      e$message <- paste0(e$message, " (at ", locus, ")")
+      stop(e)
+    })
+
+    epg$Allele1[i_locus] <- ab[1]
+    epg$Allele2[i_locus] <- ab[2]
+  }
+
+  epg
+}
---
+++ R/sample_offspring.R
@@ -0,0 +1,90 @@
+#' @title Sample offspring from two parental genotypes
+#'
+#' @param father DataFrame (see \link{sample_genotype})
+#' @param mother DataFrame (see \link{sample_genotype})
+#' @param label Character SampleName of child
+#' @details A genotype is sampled according to Mendelian inheritance.
+#' @examples
+#' # below we read an allele freqs and sample a genotype
+#' filename <- system.file("extdata","FBI_extended_Cauc.csv",package = "SimMixDNA")
+#' freqs <- read_allele_freqs(filename)
+#'
+#' # sample parents
+#' father <- sample_genotype(freqs, loci = c("D3S1358", "vWA"))
+#' mother <- sample_genotype(freqs, loci = c("D3S1358", "vWA"))
+#'
+#' # sample child
+#' child <- sample_offspring(father, mother)
+#' @export
+sample_offspring <- function(father, mother, label = "Child"){
+
+  if (!is.character(label)){
+    stop("label should be a character vector")
+  }
+  if (length(label)!=1){
+    stop("label should be a character vector of length 1")
+  }
+
+  check_genotype_df(father, "father")
+  check_genotype_df(mother, "mother")
+
+  if (!identical(father$Locus, mother$Locus)){
+    stop("father and mother should have identical locus columns")
+  }
+
+  loci <- father$Locus
+  number_of_loci <- length(loci)
+
+  a <- ifelse(sample(c(FALSE,TRUE), replace = TRUE, size = nrow(father)),father$Allele1, father$Allele2)
+  b <- ifelse(sample(c(FALSE,TRUE), replace = TRUE, size = nrow(mother)),mother$Allele1, mother$Allele2)
+
+  # sort alleles
+  for (i_locus in seq_len(number_of_loci)){
+    a_i <- a[[i_locus]]
+    b_i <- b[[i_locus]]
+
+    a_i_numeric <- as.numeric(a_i)
+    b_i_numeric <- as.numeric(b_i)
+
+    a_i_is_numeric <- as.character(a_i_numeric) == a_i
+    b_i_is_numeric <- as.character(b_i_numeric) == b_i
+
+    if (a_i_is_numeric && b_i_is_numeric && (a_i_numeric > b_i_numeric)){
+      a[i_locus] <- b_i
+      b[i_locus] <- a_i
+    }
+
+  }
+
+  epg <- data.frame("Sample Name" = rep(label, number_of_loci),
+                    Locus = loci,
+                    Allele1 = allele1,
+                    Allele2 = allele2,
+                    stringsAsFactors = FALSE, check.names = FALSE)
+
+  epg
+}
+
+check_genotype_df <- function(x, name){
+  if (!is.data.frame(x)){
+    stop(paste0(name, " should be DataFrame"))
+  }
+
+  # check that all columns are present
+  mandatory_columns <- c("Sample Name", "Locus", "Allele1", "Allele2")
+
+  for (column_name in mandatory_columns){
+    if (!(column_name %in% names(x))){
+      stop(paste0(name, " should have a ", column_name, "column"))
+    }
+  }
+
+  # check that there is a single sample name
+  if (nrow(x) == 0){
+    stop(paste0(name, " should have a at least one row"))
+  }
+
+  if (!all(x[["Sample Name"]]  == x[["Sample Name"]][1])){
+    stop(paste0(name, " should have a unique sample name"))
+  }
+}
