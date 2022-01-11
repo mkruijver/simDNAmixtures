2f1f6a1e75d6b242271983a9f4a1e56682424da6
mkruijver <maarten.kruijver@esr.cri.nz>
2022-01-12 11:10:54 +1300

    Add read_stutter_regression

---
+++ R/read_stutter_regression.R
@@ -0,0 +1,23 @@
+#' @title Reads a stutter regression file
+#'
+#' @param filename Character. Path to file.
+#' @details Reads the file from disk and returns a function that provides the fragment length (bp) for a given locus and allele.
+#' @examples
+#' filename <- system.file("extdata","GlobalFiler_SizeRegression.csv",package = "SimMixDNA")
+#' regression <- read_stutter_regression(filename)
+#' regression("vWA", 12)
+#' @export
+read_stutter_regression <- function(filename){
+
+  regression_df <- read.csv(filename, colClasses = c("character", "numeric", "numeric"))
+  regression_df_by_locus <- split(regression_df, regression_df$Locus)
+
+  f <- function(locus, allele){
+    intercept <- regression_df_by_locus[[locus]]$Intercept
+    slope <- regression_df_by_locus[[locus]]$Slope
+
+    intercept + slope * allele
+  }
+
+  f
+}
