56ffaeb9aaf81273d19c57288ee0af5fd62f0ddf
mkruijver <maarten.kruijver@esr.cri.nz>
2022-01-13 08:23:55 +1300

    Add SMASH_to_wide_table

---
+++ R/SMASH_to_wide_table.R
@@ -0,0 +1,84 @@
+#' Converts SMASH (SampleName, Marker, Allele, Size, Height) data to a wide table
+#'
+#' @param x DataFrame.
+#' @return DataFrame
+#' @export
+SMASH_to_wide_table <- function(x) {
+
+  if (!is.data.frame(x)){
+    stop("x is not a DataFrame")
+  }
+
+  mandatory_columns <- c("SampleName", "Marker", "Allele", "Size", "Height")
+  for (col in mandatory_columns){
+    if (!(col %in% names(x))){
+      stop("x does not have a ", col, " column")
+    }
+  }
+
+  # determine the maximum number of alleles at a marker across samples
+  x_by_sample_name <- split(x, x$SampleName)
+  max_number_of_alleles <- max(sapply(x_by_sample_name, function(y) max(table(y$Marker))))
+
+  wide <- get_empty_wide_table(0, max_number_of_alleles)
+
+  for (i_sample in seq_along(x_by_sample_name)){
+
+    x_sample <- x_by_sample_name[[i_sample]]
+
+    sample_name <- names(x_by_sample_name)[i_sample]
+
+    markers <- unique(x_sample$Marker)
+
+    x_sample_wide <- get_empty_wide_table(length(markers), max_number_of_alleles)
+
+    x_sample_wide$SampleName <- sample_name
+    x_sample_wide$Marker <- markers
+
+    # ensure markers are in order
+    x_sample <- x_sample[order(match(x_sample$Marker, markers)),]
+
+    # determine allele number by row number
+    allele_number_by_row_idx <- sequence(rle(x_sample$Marker)$lengths)
+
+    for (i_row in seq_len(nrow(x_sample))){
+      i_wide_row <- match(x_sample$Marker[i_row], markers)
+
+      allele_number <- allele_number_by_row_idx[i_row]
+
+      x_sample_wide[i_wide_row, 2 + allele_number] <- x_sample$Allele[i_row]
+      x_sample_wide[i_wide_row, 2 + allele_number + max_number_of_alleles] <- x_sample$Size[i_row]
+      x_sample_wide[i_wide_row, 2 + allele_number + 2 * max_number_of_alleles] <- x_sample$Height[i_row]
+    }
+
+    wide <- rbind(wide, x_sample_wide)
+  }
+
+  wide
+}
+
+get_empty_wide_table <- function(number_of_rows, number_of_alleles){
+
+  empty_character_column <- rep(NA_character_, number_of_rows)
+  empty_numeric_column <- rep(NA_real_, number_of_rows)
+
+  wide <- data.frame(SampleName = empty_character_column,
+                     Marker = empty_character_column, stringsAsFactors = FALSE)
+
+  # Allele1, Allele2, ...
+  for (i_allele in seq_len(number_of_alleles)){
+    wide[[paste0("Allele", i_allele)]] <- empty_character_column
+  }
+
+  # Size1, Size2, ...
+  for (i_allele in seq_len(number_of_alleles)){
+    wide[[paste0("Size", i_allele)]] <- empty_numeric_column
+  }
+
+  # Height1, Height2, ...
+  for (i_allele in seq_len(number_of_alleles)){
+    wide[[paste0("Height", i_allele)]] <- empty_numeric_column
+  }
+
+  wide
+}
