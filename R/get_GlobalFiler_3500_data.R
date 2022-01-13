755db3af8f7b6ca3d8f26f818252773233981640
mkruijver <maarten.kruijver@esr.cri.nz>
2022-01-14 08:25:46 +1300

    Add initial GlobalFiler 3500 data (back stutter/forward stutter only)

---
+++ R/get_GlobalFiler_3500_data.R
@@ -0,0 +1,47 @@
+#' Convenience function that loads size regression and stutter files
+#'
+#' @return Object of class list
+#' @examples
+#' gf <- get_GlobalFiler_3500_data()
+#' @export
+get_GlobalFiler_3500_data <- function() {
+
+  gf <- list()
+
+  kit_data <- get_kit_data()
+  gf$autosomal_markers <- unique(kit_data$GlobalFiler_Panel_v1$Marker[!kit_data$GlobalFiler_Panel_v1$Gender.Marker])
+
+  # size regression
+  filename_size_regression <- system.file("extdata","GlobalFiler_SizeRegression.csv",package = "SimMixDNA")
+  gf$size_regression <- read_size_regression(filename_size_regression)
+
+  # stutters
+  gf$stutters <- list()
+
+  # back stutter
+  filename_bs_exceptions <- system.file("extdata","GlobalFiler_Stutter_Exceptions_3500.csv",package = "SimMixDNA")
+  bs_exceptions <- read_stutter_exceptions(filename_bs_exceptions)
+
+  filename_bs_regression <- system.file("extdata","GlobalFiler_Stutter_3500.txt",package = "SimMixDNA")
+  bs_regression <- read_stutter_exceptions(filename_bs_regression)
+  back_stutter <- stutter_type(name = "BackStutter", delta = -1,
+                              stutter_regression = bs_regression,
+                              stutter_exceptions = bs_exceptions)
+
+  gf$stutters$BackStutter <- back_stutter
+
+  # forward stutter
+
+  filename_fs_regression <- system.file("extdata","GlobalFiler_Forward_Stutter_3500.txt",package = "SimMixDNA")
+  fs_regression <- read_stutter_exceptions(filename_fs_regression)
+  forward_stutter <- stutter_type(name = "ForwardStutter", delta = 1,
+                              stutter_regression = fs_regression)
+
+  gf$stutters$ForwardStutter <- forward_stutter
+
+
+  gf$stutter_model <- allele_specific_stutter_model(stutter_types = gf$stutters,
+                                                    size_regression = size_regression)
+
+  gf
+}
