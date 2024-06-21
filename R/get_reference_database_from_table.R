5f756108ddae2204c7645274fa7cd933570d9e30
Maarten Kruijver <maartenk@gmail.com>
2024-06-21 16:19:54 +0200

    Restore CaseNumber/SampleName for reference database

--- R/get_reference_database_from_table.R
+++ R/get_reference_database_from_table.R
@@ -1,7 +1,13 @@
 # makes a reference database from collated knowns
+# see write_knowns_as_reference_db.R
 get_reference_database <- function(x){
 
   x_by_profile <- split(x, f =  paste0(x$CaseNumber,"!",x$`Sample Name`))
 
-  .allele_tables_to_wide_references(x_by_profile)
+  case_number <- as.vector(sapply(x_by_profile, function(x) x$CaseNumber[1]))
+  sample_name <- as.vector(sapply(x_by_profile, function(x) x$`Sample Name`[1]))
+
+  db <- .allele_tables_to_wide_references(x_by_profile)
+  data.frame(CaseNumber = case_number, "Sample Name" = sample_name,
+             db, check.names = FALSE)
 }
